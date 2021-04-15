a94000386f135b8de5ca66e55801d74ba4a10780
Aquite <pavel.batalov@hotmail.com>
2021-04-15 09:46:10 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-const viewHeight = 500;
-const viewWidth = 500;
-
 
 const App = () => {
-    return (
-    <svg style={{border: "1px solid pink", width: viewWidth, height: viewHeight}}>
-        <circle cx={20} cy={20} r="5" />
-        <rect x="200" y="200" width="10" height="10" />
-        <line x1={0} y1={viewHeight} x2={150} y2={100} stroke="black" />
-    </svg>
-    );
+  fetch("https://raw.githubusercontent.com/Aquite/react-parcel-example-1/main/weather.csv")
+    .then(response => response.json())
+    .then((data) => console.log(data))
+
+  return (
+    <div>
+      <h1>Exploratory Data Analysis, Assignment 2, INFO 474 SP 2021</h1>
+    </div>
+  );
 }
 
 
